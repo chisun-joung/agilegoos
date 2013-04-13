cc9fd5454b499acfb88e043a12311ebee38c0da3
chisun-joung <csjoung@gmail.com>
2013-04-14 08:32:34 +0900

    add firt End to End Test

---
+++ AuctionSniper/test/src/com/lge/auctionsniper/test/AuctionSniperEndToEndTest.java
@@ -0,0 +1,37 @@
+package com.lge.auctionsniper.test;
+
+import com.jayway.android.robotium.solo.Solo;
+import com.lge.auctionsniper.MainActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class AuctionSniperEndToEndTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+	private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+	private final ApplicationRunner application = new ApplicationRunner();
+	private Solo solo;
+	public AuctionSniperEndToEndTest() {
+		super(MainActivity.class);
+	}
+	
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		solo = new Solo(getInstrumentation());
+	}
+	
+	public void testSniperJoinsAuctionUnitlAuctionCloses() throws Exception {
+		auction.startSellingItem();
+		application.startBiddingIn(auction,solo);
+		auction.hasReceivedJoinRequestFromSniper();
+		auction.announceClosed();
+		application.showsSniperHasLostAuction();
+	}
+	
+	@Override
+	public void tearDown() throws Exception {
+		auction.stop();
+		application.stop();
+		super.tearDown();
+	}
+}
